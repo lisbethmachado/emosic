3cedf339abee2b0b7834f0641ecce1d1d4c4dacd
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-06-25 17:19:18 -0700

    added function for sad button

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+
+// function handleSearch() {
+    $("#btn2").on("click", function(){
+
+   
+    var sad = "linkin-park"
+    var apiKey = "376609-emosic-T64LATMW";
+    var queryURL =
+        "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + sad + "&k=" + apiKey + "&verbose=1";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+        var artist = response.Similar.Info[0]
+        $("#empty").text(artist.Name)
+        $("#teaser").text(artist.wTeaser)
+        $("#youtube").empty();
+        $("#wiki").empty();
+        $("#wiki").append('<a href="artist.wUrl">' + artist.wUrl + '</a>')
+        $("#youtube").append('<a href="artist.yUrl">' + artist.yUrl + '</a>')
+          
+    });
+ });
