fa1cabd185be755b13b759ac8c065b9ad3c29071
Peiwei Xiao <pexiao@ucsd.edu>
2020-06-27 20:05:25 -0700

    fix the image

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,47 @@
 // When the user presses the happy button
 //then t
-$("#btn1").on("click", function(){
+$(function (){
+    $("#btn1").on("click", function () {
 
-$.ajax({
-    url: "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=SZA&k=376608-SaraRose-3R6F0LXH&verbose=1",
-    method: "GET",
-  }).then(function (response) {
-      console.log(response)
-      var SZA = response.Similar.Info[0]
-      $("#empty").text(SZA.Name)
-      $("#teaser").text(SZA.wTeaser)
-      $("#wiki").empty()
-      $("#youtube").empty()
-      $("#wiki").append( "<a href='" + SZA.wUrl + "'>" + SZA.wUrl + "</a>");
-      $("#youtube").append( "<a href='" + SZA.yUrl + "'>" + SZA.yUrl + "</a>");
-      console.log(SZA.wUrl)
-  })})
-  //.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
+    $.ajax({
+        url: "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=SZA&k=376608-SaraRose-3R6F0LXH&verbose=1",
+        method: "GET",
+    }).then(function (response) {
+         $("#suggestions").removeClass("hide");
+        var SZA = response.Similar.Info[0]
+        
+
+
+
+        
+        // function to render multiple artists
+        for (var i = 0; i < 6 ; i++) {
+            console.log(i)
+            var Artists = (response.Similar.Results[i])
+            
+              console.log(Artists)
+        
+            
+            // $('#artistwell').append(artistwell);
+            $("#artistwell").append("<h1 >" + Artists.Name + "</h1>");
+            $("#artistwell").append("<p>" + Artists.wTeaser + "</p>");
+            $("#artistwell").append("<a href='" + Artists.wUrl + "'>" + Artists.wUrl + "</a>");
+            $("#artistwell").append("<div>");
+            $("#artistwell").append("<a  href='" + Artists.yUrl + "'>" + Artists.yUrl + "</a>");
+            $("#artistwell").append("<div>");
+            $("#artistwell").append( "<iframe width='" +420+ "'height='" +315+ "'src='" + Artists.yUrl + "'></iframe>");
+            
+            if(i > 6 ){
+                 $("#artistwell").empty();
+                 
+                
+            }
+        
+
+
+
+
+        }
+    });
+});
+})
